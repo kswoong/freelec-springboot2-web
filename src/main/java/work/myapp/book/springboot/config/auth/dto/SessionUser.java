fae7b77d39f81d29040d0d38ded53b24095546db
Seong-Woong Kim <kswoong@mac.com>
2020-05-10 23:36:23 +0900

    work on 05/10 second time

---
+++ src/main/java/work/myapp/book/springboot/config/auth/dto/SessionUser.java
@@ -0,0 +1,19 @@
+package work.myapp.book.springboot.config.auth.dto;
+
+import lombok.Getter;
+import work.myapp.book.springboot.domain.user.User;
+
+import java.io.Serializable;
+
+@Getter
+public class SessionUser implements Serializable {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
